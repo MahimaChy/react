c06de14dce2f0d98141a585436a6fb80c4e466d7
mahimachy <mahimachy111@gmail.com>
2026-07-02 13:01:02 +0530

    Add 01-folder React project

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <div id="dad">
+        <h1 id ='child 1'>hmfmvgifkkfrg9kr9gk</h1>
+      </div>
+        <div id='chacha'></div>
+        </div>
+  )
+}
+
+export default App
